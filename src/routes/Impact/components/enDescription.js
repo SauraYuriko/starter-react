02afa0cb9e6f90878400a402fbdbbd4285dd18dd
SauraYuriko <y.saura.ce@future.co.jp>
2018-02-16 17:14:31 +0900

    after CR

---
+++ src/routes/Impact/components/enDescription.js
@@ -0,0 +1,28 @@
+const enDescription = {
+  title: 'Impact',
+  cvssSeverityV3: 'CVSS Severity (version 3.0)',
+  baseScoreV3: 'CVSS v3 Base Score',
+  vector: 'Vector',
+  impactScore: 'Impact Score',
+  exploitabilityScoreV3: 'Exploitability Score',
+  cvssMetricsV3: 'CVSS Version 3 Metrics',
+  attackVector: 'Attack Vector (AV)',
+  attackComplexity: 'Attack Complexity (AC)',
+  privilegesRequired: 'Privileges Required (PR)',
+  userInteraction: 'User Interaction (UI)',
+  scope: 'Scope (S)',
+  confidentiality: 'Confidentiality (C)',
+  integrity: 'Integrity (I)',
+  availability: 'Availability (A)',
+  cvssSeverityV2: 'CVSS Severity (version 2.0)',
+  baseScoreV2: 'CVSS v2 Base Score',
+  impactSubscore: 'Impact Subscore',
+  exploitabilitySubscoreV2: 'Exploitability Subscore',
+  cvssMetricsV2: 'CVSS Version 2 Metrics',
+  accessVector: 'Access Vector',
+  accessComplexity: 'Access Complexity',
+  authentication: 'Authentication',
+  disclosureInformation: 'Impact Type Allows unauthorized disclosure of information; Allows unauthorized modification; Allows disruption of service'
+}
+
+export default enDescription
