bb93d48345fd9d8ac9f5e272824fc7c7587cbbe7
SauraYuriko <y.saura.ce@future.co.jp>
2018-02-26 13:24:11 +0900

    fix localeReducer

--- src/modules/app.js
+++ src/modules/app.js
@@ -16,31 +16,20 @@ export const actions = {
   loadBrowsersLocale
 }
 
-// ------------------------------------
-// Action Handlers
-// ------------------------------------
-
-const ACTION_HANDLERS = {
-  [LOAD_BROWSERS_LOCALE] : (state, action) => Object.assign({}, state, action.payload),
-}
-
 
 // ------------------------------------
 // Reducer
 // ------------------------------------
-const appData = {
-  browsersLocale : window.navigator.languages[0]
-}
-
-export default function localeReducer (state = appData, action) {
-  if (state.browsersLocale === 'ja' || state.browsersLocale === 'ja-JP') {
-    Object.assign({}, state, {
+export default function localeReducer (state = {}, action) {
+  const browsersLocale = window.navigator.languages[0]
+  if (browsersLocale === 'ja' || browsersLocale === 'ja-JP') {
+    return Object.assign({}, state, {
       browsersLocale: 'ja'
     })
   }
-  else Object.assign({}, state, {
+  else {
+    return Object.assign({}, state, {
     browsersLocale: 'en'
-  })
-  const handler = ACTION_HANDLERS[action.type]
-  return handler ? handler(state, action) : state
+    })
+  }
 }
